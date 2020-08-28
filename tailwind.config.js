2c3c8024d15052803b389e4c514cbfff5e539b2e
Datner <yuval.datner.alt@gmail.com>
2020-08-28 16:15:32 +0300

    add ChatPrompt

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 module.exports = {
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-}
+	purge: [],
+	theme: {
+		extend: {
+			inset: {
+				"1/2": "50%",
+			},
+			width: {
+				"72": "18rem",
+				"84": "21rem",
+				"96": "24rem",
+			},
+			borderRadius: {
+				xl: "24px",
+			},
+			colors: {
+				blue: "#4643D3",
+			},
+		},
+	},
+	variants: {},
+	plugins: [],
+};
